d6506c48a50af2bf6c39471d1cb2d3d7ac96ca88
souleiman2 <souleiman_ayoub@hotmail.com>
2018-11-03 17:16:06 -0400

    Gui

--- sketch.js
+++ sketch.js
@@ -4,8 +4,42 @@ let c;
 let rot1, rot2;
 let h = new HyperPrism([200, 100, 300, 50, 150]);
 
+//gui variables
+let dimension = 2;
+let length = 150;
+let angularVelocity = 5;
+
+//max, min and step variables
+
+//dimension
+let dimensionMax = 10;
+let dimensionMin = 1;
+let dimensionStep = 1;
+
+
+//length
+let lengthMax = 500;
+let lengthMin = 50;
+let lengthStep = 10;
+
+//angularVariable
+let angularVelocityMax = -20;
+let angularVelocityMin = 20;
+let angularVelocityStep = 1;
+
+let gui;
+let pseudoGui;
+let hello = 0;
+
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(windowWidth, windowHeight);
+
+	pseudoGui = createGui("Length")
+	pseudoGui.addGlobals('hello')
+	//create the GUI
+	gui = createGui("Succ");
+	gui.addGlobals('dimension', 'length', 'angularVelocity', 'pseudoGui')
+
 	s = sin(PI/60);
 	c = cos(PI/60);
 	rot1 = new Matrix([[c, -s, 0, 0, 0], [s, c, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]]);
